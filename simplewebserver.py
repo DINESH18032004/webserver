58887d3f72fce45fef1b2efa89e62c5642100cca
DINESHKUMAR R <119477784+DINESH18032004@users.noreply.github.com>
2023-01-29 22:33:14 +0530

    Add files via upload

--- simplewebserver.py
+++ simplewebserver.py
@@ -1,23 +1,4 @@
-# Developing a Simple Webserver
-## AIM:
-Develop a webserver to display about top five web application development frameworks.
-
-## DESIGN STEPS:
-### Step 1: 
-HTML content creation
-### Step 2:
-Design of webserver workflow
-### Step 3:
-Implementation using Python code
-### Step 4:
-Serving the HTML pages.
-### Step 5:
-Testing the webserver
-
-## PROGRAM:
-```
 from http.server import HTTPServer, BaseHTTPRequestHandler
-
 content = """
 <!DOCTYPE html>
 <html>
@@ -36,7 +17,6 @@ content = """
 </body>
 </html>
 """
-
 class myhandler(BaseHTTPRequestHandler):
     def do_GET(self):
         print("request received")
@@ -49,10 +29,3 @@ httpd = HTTPServer(server_address,myhandler)
 print("my webserver is running...")
 httpd.serve_forever()
 #python3 simplewebserver.py
-```
-## OUTPUT:
-![GitHub Logo](webpage.png)
-
-## RESULT:
-Thus a webserver developed to display about top five web application development frameworks
-
